ccde01a68c4699b836c9192f4a582de5ef762aeb
dipsas80 <emmy.hammarstrom@gmail.com>
2022-09-16 16:00:58 +0200

    PlayerMovement Implementation
    yess

---
+++ WormsAssignmentGame/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    //player body reference
+    public Transform playerBody;
+
+
+    
+    
+
+    //ground checks & jumping
+    public float groundDrag;
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump = true;
+    public KeyCode jumpKey = KeyCode.Space;
+
+    //input vars
+    public float mouseSensitivity = 100f;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private float xRotation = 0;
+
+    public float moveSpeed = 10f;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    void Update()
+    {
+        //look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+        
+        //movement
+        MyInput();
+        SpeedControl();
+
+        //Ground checking
+        grounded = Physics.Raycast(playerBody.transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        if(grounded)
+        {
+            playerBody.GetComponent<Rigidbody>().drag = groundDrag;
+        }
+        else
+        {
+            playerBody.GetComponent<Rigidbody>().drag = 0;
+        }
+
+    }
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey) && readyToJump == true && grounded == true)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+    private void MovePlayer()
+    {
+        Vector3 moveDirection = playerBody.forward * verticalInput + playerBody.right * horizontalInput;
+
+        
+
+        if(grounded)
+        {
+            playerBody.GetComponent<Rigidbody>().AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+            
+        }
+        else if(!grounded)
+        {
+            playerBody.GetComponent<Rigidbody>().AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(playerBody.GetComponent<Rigidbody>().velocity.x, 0f, playerBody.GetComponent<Rigidbody>().velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            playerBody.GetComponent<Rigidbody>().velocity = new Vector3(limitedVel.x, playerBody.GetComponent<Rigidbody>().velocity.y, limitedVel.z);
+        }
+
+    }
+    private void Jump()
+    {
+        playerBody.GetComponent<Rigidbody>().velocity = new Vector3(playerBody.GetComponent<Rigidbody>().velocity.x, 0f, playerBody.GetComponent<Rigidbody>().velocity.z);
+
+        playerBody.GetComponent<Rigidbody>().AddForce(playerBody.transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+}
